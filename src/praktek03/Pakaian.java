95f5a382aff2a7af2489bc19ca06c12bc916f61c
JuliArlianto <32285721+JuliArlianto@users.noreply.github.com>
2018-04-07 17:39:49 +0800

    set atribut private

--- src/praktek03/Pakaian.java
+++ src/praktek03/Pakaian.java
@@ -3,9 +3,9 @@ package praktek03;
 
 
 public class Pakaian {
-    String merk;
-    String warna;
-    double harga;
+   private String merk;
+   private String warna;
+   private double harga;
     
     void cetakInfo () {
         System.out.println("Merk \t: "+merk+"\n"+
