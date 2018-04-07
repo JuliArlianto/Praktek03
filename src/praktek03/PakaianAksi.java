5cf81fa2fdda67e07f7aab1fc1cb26326219a55e
JuliArlianto <32285721+JuliArlianto@users.noreply.github.com>
2018-04-07 17:38:00 +0800

    instance , buat atribut dan panggil method

--- src/praktek03/PakaianAksi.java
+++ src/praktek03/PakaianAksi.java
@@ -4,7 +4,12 @@ package praktek03;
 
 public class PakaianAksi {
     public static void main(String[] args) {
-        
+         Pakaian p = new Pakaian();
+         p.merk="Cardinal";
+         p.warna="Blue";
+         p.harga=200000;
+         
+         p.cetakInfo();
     }
     
 }
